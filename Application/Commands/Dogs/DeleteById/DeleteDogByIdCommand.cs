8f583797c95d63fc7effe4d5240e6f2bf76ccf3f
Susu S <ssumayasofaa@gmail.com>
2023-11-27 17:40:35 +0100

    delete dog by id

---
+++ Application/Commands/Dogs/DeleteById/DeleteDogByIdCommand.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Models;
+using MediatR;
+
+namespace Application.Commands.Dogs.DeleteById
+{
+    public class DeleteDogByIdCommand : IRequest<Dog>
+    {
+        public DeleteDogByIdCommand(Guid dogId)
+        {
+            Id = dogId;
+        }
+        public Guid Id { get; set; }
+    }
+}
---
+++ Application/Commands/Dogs/DeleteById/DeleteDogByIdCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using Domain.Models;
+using Infrastructure.Database;
+using MediatR;
+
+namespace Application.Commands.Dogs.DeleteById
+{
+    public class DeleteDogByIdCommandHandler : IRequestHandler<DeleteDogByIdCommand, Dog>
+    {
+        private readonly MockDatabase _mockDatabase;
+
+        public DeleteDogByIdCommandHandler(MockDatabase mockDatabase)
+        {
+            _mockDatabase = mockDatabase;
+        }
+
+        public Task<Dog> Handle(DeleteDogByIdCommand request, CancellationToken cancellationToken)
+        {
+            var dogToDelete = _mockDatabase.Dogs.FirstOrDefault(dog => dog.Id == request.Id);
+            if (dogToDelete != null)
+            {
+                _mockDatabase.Dogs.Remove(dogToDelete);
+                return Task.FromResult(dogToDelete);
+            }
+
+            return Task.FromResult<Dog>(null);
+        }
+    }
+}
